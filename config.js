23d1d5d1b503a23646b5dce284045b33d05db4f2
Farasat Khan <farasatkahan@gmail.com>
2017-06-11 14:35:53 +0500

    comitted

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	consumer_key:         '6mGJM37I1FXclkrHppssRQaRd',
-  	consumer_secret:      'nIQqN1Dann37b3zqCjPbQOXHCtUKTfX97vtb3UGYExdFgcqhN1',
-  	access_token:         '872769744826454016-qUrO8MP9gDjJMVR3pCywLNVjl7vVUSd',
-  	access_token_secret:  'uNRtNdczrgxoFrqfVz0UgkshS63bw73btEvOVHDVLTPbh',
-}
--- configNY.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  url: "https://api.nytimes.com/svc/topstories/v2/home.json",
-  qs: {
-    'api-key': 'c299f896b5bd4900bd44d00e24d6e5bd'
-  },
-}
